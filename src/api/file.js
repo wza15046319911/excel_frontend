901a0e86a0d5e1d9d5128ffb485d8a0f43905f2e
Zian Wang <wza15046319911@github.com>
2022-12-06 18:05:01 +1100

    change to basic create-react-app

--- src/api/file.js
+++
@@ -1,7 +0,0 @@
-
-export const uploadFile = async(file) => {
-    const formData = new FormData();
-    formData.append('file', file);
-    const res = await fetch('/api/upload', {method: "POST", body: formData})
-    return await res.json();
-}
